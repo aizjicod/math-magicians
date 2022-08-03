c31ac896352f39101356209262cbb34aa5dc5747
aizijiCOD <aletv1501@gmail.com>
2022-08-03 14:48:16 -0400

    add functionality to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,59 +1,68 @@
 import React from 'react';
 import './Calculator.css';
-
-const Utility = () => (
-  <div className="utility">
-    <button type="button">AC</button>
-    <button type="button">+/-</button>
-    <button type="button">%</button>
-  </div>
-);
-
-const LeftSide = () => (
-  <div className="leftSide">
-    <button type="button">÷</button>
-    <button type="button">*</button>
-    <button type="button">-</button>
-    <button type="button">+</button>
-    <button type="button">=</button>
-  </div>
-);
-
-const Numpad = () => (
-  <div className="numpad">
-    <button type="button">7</button>
-    <button type="button">8</button>
-    <button type="button">9</button>
-    <button type="button">4</button>
-    <button type="button">5</button>
-    <button type="button">6</button>
-    <button type="button">1</button>
-    <button type="button">2</button>
-    <button type="button">3</button>
-    <button type="button" className="numpad-0">0</button>
-    <button type="button">.</button>
-  </div>
-);
-
-const Pad = () => (
-  <div className="pad">
-    <Utility />
-    <Numpad />
-    <LeftSide />
-  </div>
-);
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+      err: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const {
+      total, next, operation, err,
+    } = calculate(this.state, e.target.value);
+    this.setState({
+      total, next, operation, err,
+    });
   }
 
   render() {
+    const {
+      total, next, operation, err,
+    } = this.state;
     return (
       <div id="Calculator">
-        <div id="result">results</div>
-        <Pad />
+        <div id="result">
+          { next
+        || operation
+        || total
+        || err
+        || 0}
+        </div>
+        <div className="pad">
+          <div className="utility">
+            <button type="button" value="AC" onClick={this.handleClick}>AC</button>
+            <button type="button" value="+/-" onClick={this.handleClick}>+/-</button>
+            <button type="button" value="%" onClick={this.handleClick}>%</button>
+          </div>
+          <div className="numpad">
+            <button type="button" value="7" onClick={this.handleClick}>7</button>
+            <button type="button" value="8" onClick={this.handleClick}>8</button>
+            <button type="button" value="9" onClick={this.handleClick}>9</button>
+            <button type="button" value="4" onClick={this.handleClick}>4</button>
+            <button type="button" value="5" onClick={this.handleClick}>5</button>
+            <button type="button" value="6" onClick={this.handleClick}>6</button>
+            <button type="button" value="1" onClick={this.handleClick}>1</button>
+            <button type="button" value="2" onClick={this.handleClick}>2</button>
+            <button type="button" value="3" onClick={this.handleClick}>3</button>
+            <button type="button" value="0" onClick={this.handleClick} className="numpad-0">0</button>
+            <button type="button" value="." onClick={this.handleClick}>.</button>
+          </div>
+          <div className="leftSide">
+            <button type="button" value="÷" onClick={this.handleClick}>÷</button>
+            <button type="button" value="x" onClick={this.handleClick}>x</button>
+            <button type="button" value="-" onClick={this.handleClick}>-</button>
+            <button type="button" value="+" onClick={this.handleClick}>+</button>
+            <button type="button" value="=" onClick={this.handleClick}>=</button>
+          </div>
+        </div>
       </div>
     );
   }
