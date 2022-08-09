1667a5d3a910d961a934ac3c1f9aacca5057976c
aizijiCOD <aletv1501@gmail.com>
2022-08-09 17:34:36 -0400

    add Routes and styling to the page

---
+++ src/pages/calculatorPage.js
@@ -0,0 +1,10 @@
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <div id="App">
+    <h2>Let&#39;s do some Math</h2>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+const Home = () => (
+  <div id="home">
+    <h2>Welcome to our page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Iure illo earum
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Suscipit vitae ut, quo, similique l
+      ibero nihil eius cupiditate nostrum
+      porro minus repellendus quaerat delectus harum!
+      Perferendis, voluptates officiis. Magni, repellendus cum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Iure illo earum
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Suscipit vitae ut, quo, similique l
+      ibero nihil eius cupiditate nostrum
+      porro minus repellendus quaerat delectus harum!
+      Perferendis, voluptates officiis. Magni, repellendus cum.
+    </p>
+  </div>
+);
+
+export default Home;
